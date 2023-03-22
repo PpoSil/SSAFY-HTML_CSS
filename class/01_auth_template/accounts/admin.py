e339f8cb03db00442d038eaa0e363082b26ccb2b
PpoSil <117746998+PpoSil@users.noreply.github.com>
2023-03-22 15:53:57 +0900

    Delete class/01_auth_template directory

--- class/01_auth_template/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-# Register your models here.
-admin.site.register(User, UserAdmin)
-
-# 여기는 안 해도 무방! 선택임!!
